5595b6e3b8d578ef04714c75ddf14727de807e63
mcarlomagno <marcoscarlomagno2@gmail.com>
2020-07-10 10:26:38 -0300

    add: sql queries

--- src/database/database.sql
+++ src/database/database.sql
@@ -18,3 +18,25 @@ INSERT INTO users (nombre, email, password) VALUES (
 );
 
 SELECT id FROM users WHERE email = 'jon@doe.com' AND password = crypt('jon', password);
+
+/* 10/07 */
+CREATE TABLE roles (
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(40)
+);
+
+CREATE TABLE roles_users (
+    id SERIAL PRIMARY KEY,
+    id_user INTEGER,
+	id_rol INTEGER,
+	FOREIGN KEY (id_user) REFERENCES users (id),
+	FOREIGN KEY (id_rol) REFERENCES roles (id)
+);
+
+ALTER TABLE users 
+ADD COLUMN apellido VARCHAR(40),
+ADD COLUMN is_coworker BOOLEAN,
+ADD COLUMN dni VARCHAR(10),
+ADD COLUMN fecha_nacimiento DATE,
+ADD COLUMN direccion VARCHAR(100),
+ADD COLUMN celular VARCHAR(40);
