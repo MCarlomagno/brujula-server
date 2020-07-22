4469a87ab781ce85665d02f9defa63d03cff9a34
Marcos Carlomagno <marcoscarlomagno1@gmail.com>
2020-07-22 09:18:51 -0300

    fix: coworkers create service

--- src/controllers/coworkers.controller.ts
+++ src/controllers/coworkers.controller.ts
@@ -26,6 +26,7 @@ export async function getCoworkers(req: any, res: any) {
     const query = `SELECT u.nombre, u.apellido, u.email, p.horas_sala, u.horas_sala_consumidas
                     FROM users u INNER JOIN plans p ON u.id_plan = p.id
                     WHERE is_coworker = true AND (LOWER(u.nombre) LIKE '%' || LOWER($3) || '%' OR LOWER(u.apellido) LIKE '%' || LOWER($3) || '%' OR LOWER(u.email) LIKE '%' || LOWER($3) ||'%')
+                    ORDER BY u.created_at DESC
                     LIMIT $1 OFFSET ($2::numeric * $1)`;
     const queryResult = await pool.query(query, [itemsPerPage, pageNumber, filter]);
     res.json(queryResult.rows);
@@ -46,12 +47,21 @@ export async function getCoworkersCount(req: any, res: any) {
 export async function createCoworker(req: any, res: any) {
     const coworker = req.body.coworker;
     const usersPuestos = req.body.usersPuestos;
+    const selectedPlan = req.body.selectedPlan;
 
     // creating random password for new user
     const pass = Math.random().toString(36).slice(-8);
     try {
+
+        if(selectedPlan.is_custom) {
+            const insertPlanQuery = "INSERT INTO plans (horas_sala, is_custom, nombre, descripcion) VALUES ($1, $2, $3, $4) RETURNING id;";
+            const insertPlanQueryResult = await pool.query(insertPlanQuery, [selectedPlan.horas_sala, selectedPlan.is_custom, selectedPlan.nombre, selectedPlan.descripcion]);
+            selectedPlan.id = insertPlanQueryResult.rows[0].id;
+            coworker.id_plan = insertPlanQueryResult.rows[0].id;
+        }
+
         // insert query to users
-        const insertCoworkerQuery = "INSERT INTO users (nombre, apellido, email, password, is_coworker, id_grupo, dni, fecha_nacimiento, direccion, celular, id_plan, horas_sala_consumidas ) VALUES ($1, $2, $3, crypt($4, gen_salt('bf')), $5, $6, $7, $8, $9, $10, $11, $12) RETURNING id;";
+        const insertCoworkerQuery = "INSERT INTO users (nombre, apellido, email, password, is_coworker, id_grupo, dni, fecha_nacimiento, direccion, celular, id_plan, horas_sala_consumidas, created_at) VALUES ($1, $2, $3, crypt($4, gen_salt('bf')), $5, $6, $7, $8, $9, $10, $11, $12, CURRENT_TIMESTAMP) RETURNING id;";
         const queryResult = await pool.query(insertCoworkerQuery, [coworker.nombre, coworker.apellido, coworker.email, pass, coworker.is_coworker, coworker.id_grupo, coworker.dni, coworker.fecha_nacimiento, coworker.direccion, coworker.celular, coworker.id_plan, coworker.horas_sala_consumidas]);
 
         if (coworker.is_leader) {
@@ -74,12 +84,12 @@ export async function createCoworker(req: any, res: any) {
             const horaHasta = usersPuestos.hora_hasta.hours + ':' + usersPuestos.hora_hasta.minutes;
 
             // insert query to users_puestos
-            const insertUsersPuestosQuery = "INSERT INTO users_puestos (id_user, id_puesto, hora_desde, hora_hasta, fecha_desde, fecha_hasta, lunes, martes, miercoles, jueves, viernes) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)";
+            const insertUsersPuestosQuery = "INSERT INTO users_puestos (id_user, id_puesto, hora_desde, hora_hasta, fecha_desde, fecha_hasta, lunes, martes, miercoles, jueves, viernes, sabado) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)";
             const queryResultUsersPuestos = await pool.query(insertUsersPuestosQuery,
                 [idUser, idPuesto, horaDesde,
                     horaHasta, usersPuestos.fecha_desde, usersPuestos.fecha_hasta,
                     usersPuestos.dias[0], usersPuestos.dias[1], usersPuestos.dias[2],
-                    usersPuestos.dias[3], usersPuestos.dias[4]]);
+                    usersPuestos.dias[3], usersPuestos.dias[4], usersPuestos.dias[5]]);
         }
 
 
